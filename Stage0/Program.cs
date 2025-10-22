847040ac3ae166b63e4ceceec88a81ba4fda8655
Lior h <lior.halimi@yahoo.fr>
2025-10-22 19:45:38 +0300

    initial commit

---
+++ Stage0/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Stage0;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter your user name");
+        string userName = Console.ReadLine()!;
+        Console.WriteLine($"{userName}, welcome to my first application");
+    }
+}
