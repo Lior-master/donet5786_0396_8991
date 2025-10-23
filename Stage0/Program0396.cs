a980cd43c16d9a3fc3ff9057edc6d25a2f968473
Lior h <lior.halimi@yahoo.fr>
2025-10-23 20:55:15 +0300

    initial code + Welcome functions

---
+++ Stage0/Program0396.cs
@@ -0,0 +1,19 @@
+﻿namespace Stage0;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome0396();
+        Welcome8991();
+        Console.ReadKey();
+    }
+
+    static partial void Welcome8991();
+    private static void Welcome0396()
+    {
+        Console.WriteLine("Enter your name: ");
+        var x = Console.ReadLine()!;
+        Console.WriteLine("{0}, wlecome to my first console application", x);
+    }
+}
