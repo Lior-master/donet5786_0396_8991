fa55e9484ed33cf809b3897b073cd901321cf3ef
Lior h <lior.halimi@yahoo.fr>
2025-10-29 17:57:16 +0200

    different inclomplete enums

---
+++ DalFacade/DO/Enums.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
+
+public enum OrderStatus
+{
+
+}
+
+public enum DeliveryTransport
+{
+    /// <summary>
+    /// Delivery by drone
+    /// </summary>
+    Bicycle,
+
+}
+
+public enum OrderType
+{
+
+
+}
+
+public enum PriiorityLevel
+{
+
+}
+
+public enum FragilityLevel
+{
+
+}
---
+++ DalFacade/DO/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
+
+public record Order
+{
+    int Id,
+    OrderType Type,
+    string? Description = null;
+    int CustomerId;
+    DateTime ? OrderDate;
+    OrderStatus ? 
+    DeliveryTransport TransportMethod,
+    string ?  Address = null;
+    string? City = null;
+    string? Country = null;
+    string? ZipCode = null;
+}
