51cfa32b7907df7bbf07ccfe199d9a1277bf68d6
Lior h <lior.halimi@yahoo.fr>
2025-10-30 20:17:42 +0200

    start of the stage1 order.cs

---
+++ DalList/Config.cs
@@ -0,0 +1,26 @@
+﻿namespace Dal;
+
+internal static class Config
+(
+    
+    int NextOrderId,
+    int NextDeliveryId,
+    DateTime Clock,
+    int BossId,
+    string BossPasword, // a redefinir tosefet
+    string? CompanyAdress = null,
+    double? Latitude = null,
+    double? Longitude = null,
+    double? MaxDistance = null,
+    double SpeedOfCar,
+    double SpeedOfMotorBike,
+    double speedOfBike,
+    double SpeedOfWalking,
+    TimeSpan MaxDeliveryTimeRange,
+    TimeSpan RiskRange,
+    TimeSpan InactivityTime
+)
+{
+
+}
+
