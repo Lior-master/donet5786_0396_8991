423bd4bc996318246d3eead9285f769eb81621ca
Lior h <lior.halimi@yahoo.fr>
2025-11-02 02:25:38 +0200

    chap 7

---
+++ DalFacade/DalApi/IConfig.cs
@@ -0,0 +1,14 @@
+﻿using DO;
+
+namespace DalApi;
+
+public interface IConfig
+{
+    void Create(Delivery item); //Creates new entity object in DAL
+    Config? Read(int id); //Reads entity object by its ID 
+    List<T> ReadAll(); //stage 1 only, Reads all entity objects
+    void Update(T item); //Updates entity object
+    void Delete(int id); //Deletes an object by its Id
+    void DeleteAll(); //Delete all entity objects
+
+}
--- DalList/Config.cs
+++ DalList/Config.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dal;
 
-internal static class Config
+public static class Config
 {
 
     internal const int startOrderId = 1000;
@@ -14,19 +14,19 @@ internal static class Config
 
     internal static DateTime Clock { get; set; } = DateTime.Now;
 
-    internal static int BossId { get; set; } = 1;
-    internal static string BossPassword { get; set; } = "1234"; // modifiable plus tard
+    internal static int BossId { get; set; } = 0;
+    internal static string BossPassword { get; set; } = string.Empty; // modifiable plus tard
 
-    internal static string CompanyAddress { get; set; } = "Tel Aviv, Israel";
-    internal static double Latitude { get; set; } = 32.0853;
-    internal static double Longitude { get; set; } = 34.7818;
+    internal static string CompanyAddress { get; set; } = string.Empty;
+    internal static double Latitude { get; set; } = 0;
+    internal static double Longitude { get; set; } = 0;
 
-    internal static double SpeedOfCar { get; set; } = 50.0;
-    internal static double SpeedOfMotorBike { get; set; } = 40.0;
-    internal static double SpeedOfBike { get; set; } = 15.0;
-    internal static double SpeedOfWalking { get; set; } = 5.0;
+    internal static double SpeedOfCar { get; set; } = 0;
+    internal static double SpeedOfMotorBike { get; set; } = 0;
+    internal static double SpeedOfBike { get; set; } = 0;
+    internal static double SpeedOfWalking { get; set; } = 0;
 
-    internal static double MaxDistance { get; set; } = 10.0; 
+    internal static double MaxDistance { get; set; } = 0; 
     internal static TimeSpan MaxDeliveryTimeRange { get; set; } = TimeSpan.FromHours(3);
     internal static TimeSpan RiskRange { get; set; } = TimeSpan.FromHours(2);
     internal static TimeSpan InactivityTime { get; set; } = TimeSpan.FromHours(1);
