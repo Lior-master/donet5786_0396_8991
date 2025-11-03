d63299bbebb55a441cf2ee181adde5bc0c79f571
BEN NAKACHE <nakacheben5@gmail.com>
2025-11-03 14:50:46 +0200

    DeliveryImplementation

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -6,4 +6,7 @@ internal static class DataSource
     internal static List<DO.Order> Orders { get; } = new();
     internal static List<DO.Delivery> Deliveries { get; } = new();
     internal static List<DO.Courier> Couriers { get; } = new();
+
+     internal static List<DalList.Config> Configs { get; } = new();
+
 }
