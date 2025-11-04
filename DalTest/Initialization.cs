c6a3c7c29b3a0489c6b158e8649706de81b9f738
Lior h <lior.halimi@yahoo.fr>
2025-11-04 11:15:56 +0200

    Initialization.cs to complet

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,19 @@
+﻿namespace DalTest;
+
+using DO;
+using DalApi;
+
+public static class Initialization
+{
+    private static ICourier? s_dalCourier;
+    private static IDelivery? s_dalDelivery;
+    private static IOrder? s_dalOrder;
+    private static IConfig? s_dalConfig;
+
+    private static readonly Random s_rand = new();
+
+    private static void createCouriers()
+    {
+
+    }
+}
