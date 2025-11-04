ed534b6a39a8a6a024b84855b7ba98d355734f61
Lior h <lior.halimi@yahoo.fr>
2025-11-04 15:10:57 +0200

    createcourier initialization

--- DalTest/Initialization.cs
+++ DalTest/Initialization.cs
@@ -14,6 +14,20 @@ public static class Initialization
 
     private static void createCouriers()
     {
-
+        for (int i = 0; i < 25; i++)
+        {
+            Courier courier = new Courier
+            (
+                Id: 0,
+                Name: $"Courier_{i + 1}",
+                Phone: $"+100000000{i + 1:D2}",
+                Email: $"Courier_{i + 1}@gmail.com",
+                Password: "password",
+                IsActive: s_rand.Next(0, 5) % 2 == 0,
+                Transport: (DeliveryTransport)(s_rand.Next(0, 4)),
+                StartDate: DateTime.Now.AddDays(-s_rand.Next(0, 365)),
+                MaxDistance: s_rand.Next(0, 20));
+            s_dalCourier!.Create(courier);
+        }
     }
 }
