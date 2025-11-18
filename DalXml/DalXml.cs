65dcf767c0b0b35ef02d042ac3c1b129f182fe9a
Lior h <lior.halimi@yahoo.fr>
2025-11-18 23:26:44 +0200

    DalXml.cs

---
+++ DalXml/DalXml.cs
@@ -0,0 +1,22 @@
+﻿using DalApi;
+
+namespace Dal;
+
+sealed public class DalXml : IDal
+{
+    public IOrder Order { get; } = new OrderImplementation();
+
+    public ICourier Courier { get; } = new CourierImplementation();
+
+    public IDelivery Delivery { get; } = new DeliveryImplementation();
+
+    public IConfig Config { get; } = new ConfigImplementation();
+
+    public void ResetDB()
+    {
+        Order.DeleteAll();
+        Courier.DeleteAll();
+        Delivery.DeleteAll();
+        Config.Reset();
+    }
+}
