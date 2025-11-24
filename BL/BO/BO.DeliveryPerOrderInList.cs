8234aaead7eff5375cfa55904385155c36b9d7c9
BEN NAKACHE <nakacheben5@gmail.com>
2025-11-24 15:50:11 +0200

    BO.DeliveryPerOrderInList

--- BL/BO/BO.DeliveryPerOrderInList.cs
+++ BL/BO/BO.DeliveryPerOrderInList.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    internal class BO
+    public class DeliverPerOrderInList
     {
+        public int DeliveryId { get; set; }
+        public int CourierId { get; set; }
+        public string Name { get; set; }
+        public OrderType OrderType { get; set; }
+        public DateTime PickupTime { get; set; }
+
+        public OrderStatus OrderStatus { get; set; }
+        public DateTime ArrivalTime { get; set; }
     }
 }
