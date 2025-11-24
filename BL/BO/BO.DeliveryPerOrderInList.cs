c267c4025092b7caa18f4fe62b153ccac3da0a45
BEN NAKACHE <nakacheben5@gmail.com>
2025-11-24 16:19:48 +0200

    BO.OpenInList

--- BL/BO/BO.DeliveryPerOrderInList.cs
+++ BL/BO/BO.DeliveryPerOrderInList.cs
@@ -8,13 +8,12 @@ namespace BO
 {
     public class DeliverPerOrderInList
     {
-        public int DeliveryId { get; set; }
-        public int CourierId { get; set; }
-        public string Name { get; set; }
-        public OrderType OrderType { get; set; }
-        public DateTime PickupTime { get; set; }
-
-        public OrderStatus OrderStatus { get; set; }
-        public DateTime ArrivalTime { get; set; }
+        public int DeliveryId { get; init; }
+        public int CourierId { get; init; }
+        public string Name { get; init; }
+        public OrderType OrderType { get; init; }
+        public DateTime PickupTime { get; init; }
+        public OrderStatus OrderStatus { get; init; }
+        public DateTime ArrivalTime { get; init; }
     }
 }
--- BL/BO/BO.OpenOrderInList.cs
+++ BL/BO/BO.OpenOrderInList.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace BO
 {
-    internal class BO
+    public class OpenOrderInList
     {
+        public int CourierId { get; init; }
+        public int OrderId { get; init; }
+        public OrderType OrderType { get; init; }
+        public FragilityLevel? Fragility { get; init; }
+        public string CustomerAddress { get; init; }
+        public double BirdDistance { get; init; }
+        public double Distance { get; init; }
+        public TimeSpan AddedTime { get; init; }
+        public ScheduleStatus ScheduleStatus { get; init; }
+        public TimeSpan EstimatedDeliveryTime { get; init; }
+        public DateTime MaxDeliveredTime { get; init; }
     }
 }
