2d8c89095555af4d928a894884368982a18461c3
Lior h <lior.halimi@yahoo.fr>
2025-11-24 16:00:23 +0200

    BO.Order.cs without ToString()

--- BL/BO/BO.Order.cs
+++ BL/BO/BO.Order.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BO;
 
-namespace BO
+public class Order
 {
-    internal class BO
-    {
-    }
+    public int Id { get; init; }
+    public OrderType Type { get; set; }
+    public string? OrderDescription { get; set; }
+    public string CustomerAdress { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Distance { get; set; }
+    public string CustomerName { get; set; }
+    public string CustomerPhone { get; set; }
+    public double? Weight { get; set; }
+    public FragilityLevel? Fragility { get; set; }
+    public double? Volume { get; set; }
+    public DateTime OrderDate { get; init; }
+    public DateTime? ArrivalDateEstimeted { get; init; }
+    public DateTime? ArrivalDateMax { get; init; }
+    public OrderStatus Status { get; init; }
+    public ScheduleStatus ScheduleStatus { get; init; }
+    public TimeSpan ArrivalTimeEstimeted { get; init; }
+    public List<BO.DeliveryPerOrderInList>? DeliveriesPerOrder { get; init; }
 }
