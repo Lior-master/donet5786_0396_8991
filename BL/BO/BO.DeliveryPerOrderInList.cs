b015e6858dc9469c832e6538f4edb18cafaf4908
Lior h <lior.halimi@yahoo.fr>
2025-11-24 16:18:01 +0200

    Bo.orderinlist.cs without toString

--- BL/BO/BO.DeliveryPerOrderInList.cs
+++ BL/BO/BO.DeliveryPerOrderInList.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace BO;
 
-namespace BO
+public class DeliverPerOrderInList
 {
-    public class DeliverPerOrderInList
-    {
-        public int DeliveryId { get; set; }
-        public int CourierId { get; set; }
-        public string Name { get; set; }
-        public OrderType OrderType { get; set; }
-        public DateTime PickupTime { get; set; }
+    public int DeliveryId { get; set; }
+    public int CourierId { get; set; }
+    public string Name { get; set; }
+    public OrderType OrderType { get; set; }
+    public DateTime PickupTime { get; set; }
 
-        public OrderStatus OrderStatus { get; set; }
-        public DateTime ArrivalTime { get; set; }
-    }
+    public OrderStatus OrderStatus { get; set; }
+    public DateTime ArrivalTime { get; set; }
 }
