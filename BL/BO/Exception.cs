15345f6a6ab0c7bb19c974caec50bd59fe89b068
Lior h <lior.halimi@yahoo.fr>
2025-11-25 22:50:33 +0200

    Exception.cs

---
+++ BL/BO/Exception.cs
@@ -0,0 +1,8 @@
+﻿namespace BO;
+
+[Serializable]
+public class BLTemporaryNotAvailableException : Exception
+{
+    public BLTemporaryNotAvailableException(string? message) : base(message) { }
+    public BLTemporaryNotAvailableException(string message,Exception innerException) : base(message, innerException) { }
+}
