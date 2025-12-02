8ccb34bfffb84a74b18ee7bb80aea2c455b0d6c3
Lior h <lior.halimi@yahoo.fr>
2025-12-02 22:35:07 +0200

    BirdDistance correction to accept nullable value and return -1 if there is a null value

--- BL/Helpers/Tools.cs
+++ BL/Helpers/Tools.cs
@@ -23,18 +23,22 @@ internal static class Tools
         return sb.ToString();
     }
 
-    public static double BirdDistance(double lat1, double lon1, double lat2, double lon2)
+    // Accept nullable parameters and return null if any parameter is null.
+    public static double BirdDistance(double? lat1, double? lon1, double? lat2, double? lon2)
     {
+        if (!lat1.HasValue || !lon1.HasValue || !lat2.HasValue || !lon2.HasValue)
+            return -1;
+
         const double R = 6371; // earth radius in kilometers
 
-        double dLat = ToRadians(lat2 - lat1);
-        double dLon = ToRadians(lon2 - lon1);
+        double dLat = ToRadians(lat2.Value - lat1.Value);
+        double dLon = ToRadians(lon2.Value - lon1.Value);
 
-        lat1 = ToRadians(lat1);
-        lat2 = ToRadians(lat2);
+        double la1 = ToRadians(lat1.Value);
+        double la2 = ToRadians(lat2.Value);
 
         double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
-                   Math.Cos(lat1) * Math.Cos(lat2) *
+                   Math.Cos(la1) * Math.Cos(la2) *
                    Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
 
         double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
