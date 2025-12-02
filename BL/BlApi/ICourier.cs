5702fcecf0cf4ea550f6abfb1425b7a0a5c7ff72
Lior h <lior.halimi@yahoo.fr>
2025-12-02 22:04:53 +0200

    minor correction in Icourier.cs

--- BL/BlApi/ICourier.cs
+++ BL/BlApi/ICourier.cs
@@ -1,16 +1,15 @@
-﻿namespace BLApi
-{
-    public interface ICourier
-    {
-        void Create(BO.Courier courier);
-        BO.Courier Read(int id);
-        IEnumerable<BO.CourierInList> ReadAll();
-        void Update(BO.Courier courier);
-        void Delete(int id);
+﻿namespace BLApi;
 
-        IEnumerable<BO.OpenOrderInList> GetEligibleOrders(int courierId);
-        void TakeOrder(int courierId, int orderId);
-        BO.OrderInProgress? CurrentOrder(int courierId);
-        IEnumerable<BO.ClosedDeliveryInList> GetHistory(int courierId);
-    }
+public interface ICourier
+{
+    void Create(BO.Courier courier);
+    BO.Courier Read(int id);
+    IEnumerable<BO.CourierInList> ReadAll();
+    void Update(BO.Courier courier);
+    void Delete(int id);
+
+    IEnumerable<BO.OpenOrderInList> GetEligibleOrders(int courierId);
+    void TakeOrder(int courierId, int orderId);
+    BO.OrderInProgress? CurrentOrder(int courierId);
+    IEnumerable<BO.ClosedDeliveryInList> GetHistory(int courierId);
 }
