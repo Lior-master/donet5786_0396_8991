0d946a2087b15a2900f2626ea772d393044e0fa3
Lior h <lior.halimi@yahoo.fr>
2025-12-03 23:31:25 +0200

    IAdmins.cs final commit

--- BL/BlApi/IAdmin.cs
+++ BL/BlApi/IAdmin.cs
@@ -2,11 +2,10 @@
 
 public interface IAdmin
 {
-    void ResetDB();                     // Réinitialiser toute la DB
-    void InitializeDB();                // Ré-initialiser avec les données de départ
-    DateTime GetClock();                // Lire l’horloge de la simulation
-    void ForwardClock(BO.TimeUnit unit); // Avancer l’horloge (minute, heure, jour, etc.)
-
-    BO.Config GetConfig();              // Lire la config (BO.Config)
-    void SetConfig(BO.Config config);   // Mettre à jour la config
+    void ResetDB();                     // Reset all DB
+    void InitializeDB();                // Reset with initialize DB
+    DateTime GetClock();                // Read the clock
+    void ForwardClock(BO.TimeUnit unit); // to forward the clock
+    BO.Config GetConfig();              // Read the config
+    void SetConfig(BO.Config config);   // to update the config
 }
